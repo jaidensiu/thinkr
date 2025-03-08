d720dba1c3d46beec16880e470228e2a7e8c9d17
Ray Ho <108425957+Raywnh@users.noreply.github.com>
2025-03-07 18:17:05 -0800

    feat: add jest

---
+++ backend/jest.config.js
@@ -0,0 +1,28 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    roots: ['src'],
+    testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+    transform: {
+      '^.+\\.ts$': 'ts-jest',
+    },
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    collectCoverageFrom: [
+      'src/**/*.ts',
+      '!src/**/*.test.ts',
+      '!src/**/*.spec.ts',
+      '!src/**/__tests__/**',
+      '!**/node_modules/**'
+    ],
+    coverageThreshold: {
+      global: {
+        branches: 100,
+        functions: 100,
+        lines: 100,
+        statements: 100
+      }
+    },
+    verbose: true
+  };
